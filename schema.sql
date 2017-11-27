8976df74546f571fbe7ceb569dfa38d97a018795
Jon Warbrick <jw35@cam.ac.uk>
2017-11-27 10:17:01 +0000

    Add 'production' versions of siti-vm load files

---
+++ schema.sql
@@ -0,0 +1,48 @@
+
+CREATE EXTENSION postgis;
+
+/*
+   "request_data": [
+        {
+            "Bearing": "300",
+            "DataFrameRef": "1",
+            "DatedVehicleJourneyRef": "119",
+            "Delay": "-PT33S",
+            "DestinationName": "Emmanuel St Stop E1",
+            "DestinationRef": "0500CCITY487",
+            "DirectionRef": "OUTBOUND",
+            "InPanic": "0",
+            "Latitude": "52.2051239",
+            "LineRef": "7",
+            "Longitude": "0.1242290",
+            "Monitored": "true",
+            "OperatorRef": "SCCM",
+            "OriginAimedDepartureTime": "2017-10-25T23:14:00+01:00",
+            "OriginName": "Park Road",
+            "OriginRef": "0500SSAWS023",
+            "PublishedLineName": "7",
+            "RecordedAtTime": "2017-10-25T23:59:48+01:00",
+            "ValidUntilTime": "2017-10-25T23:59:48+01:00",
+            "VehicleMonitoringRef": "SCCM-19597",
+            "VehicleRef": "SCCM-19597",
+            "acp_id": "SCCM-19597",
+            "acp_lat": 52.2051239,
+            "acp_lng": 0.124229,
+            "acp_ts": 1508972388
+        },
+*/
+
+CREATE TABLE siri_vm (
+    id                       BIGSERIAL PRIMARY KEY,
+    acp_id                   TEXT,
+    location4d               GEOGRAPHY(POINTZM,4326),
+    acp_ts                   TIMESTAMP WITH TIME ZONE,
+    info                     JSONB,
+    file_ts                  TIMESTAMP WITH TIME ZONE
+   );
+
+
+CREATE INDEX siri_vm_acp_id ON siri_vm (acp_id);
+CREATE INDEX siri_vm_location4d ON siri_vm USING GIST (location4d);
+CREATE INDEX siri_vm_acp_ts ON siri_vm (acp_ts);
+CREATE INDEX siri_vm_info ON siri_vm USING GIN (info);
