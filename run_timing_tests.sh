3d9b3b41ed0b8c4d78f51d5b030faa2b029cbf76
Jon Warbrick <jw35@cam.ac.uk>
2017-12-05 12:20:35 +0000

    Reworked run_timing_tests.sh to expect a markdown template
    Rather than just execuiting non-comemnt non-blank lines from
    its input file, run_timing_tests.sh now expects a Markdown-
    formatted skeleton as input. It copies most of this through to
    its output, except for lines within '!!!'-delimited
    blocks which it passes to run_timing_test.sh and inserts the
    result into its output wthin '```'-delimited blocks

--- run_timing_tests.sh
+++ run_timing_tests.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-while read line
+in_quoted='false'
+
+while IFS= read -r
 do
-  if [[ -n "${line}" -a "${line}"" != #* ) ]]
+
+  # Skip actual comments
+  if [[ "${REPLY}" == '#'* ]]
   then
-    ./run_timing_tests.sh "${line}"
+    :
+
+  # Track quoted block state
+  elif [[ "${REPLY}" == '"""' ]]
+  then
+    if [[ "${in_quoted}" == 'true' ]]
+    then
+      in_quoted='false'
+    else
+      in_quoted='true'
+    fi
+    # ..and convert to backtics
+  echo '```'
+
+  # Execute REPLYs inside quoted block
+  elif [[ "${in_quoted}" == 'true' ]]
+  then
+    ./run_timing_test.sh "${REPLY}"
+
+  # And otherwise copy the REPLY through
+  else
+    echo "${REPLY}"
+
   fi
+
 done < $1
 
