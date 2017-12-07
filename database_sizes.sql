b6f70ba0d45673f2e24a2a814ca2163316f077ac
Jon Warbrick <jw35@cam.ac.uk>
2017-12-07 12:11:44 +0000

    Add as sql script to display siri_vm table sizes

---
+++ database_sizes.sql
@@ -0,0 +1,51 @@
+-- Print size stats for the various siri_vm databases
+
+\d siri_vm
+\d siri_vm2
+\d siri_vm_3
+
+SELECT
+    relname as tablename,
+    to_char(reltuples::bigint, '999,999,999') AS records
+FROM pg_class
+where relname in (
+    select tablename
+    from pg_tables
+    where tablename like 'siri_v%'
+)
+order by tablename;
+
+select
+    tablename,
+    pg_size_pretty(pg_relation_size(tablename::text,'main')) as Main,
+    pg_size_pretty(pg_relation_size(tablename::text,'fsm')) as Free_space_map,
+    pg_size_pretty(pg_relation_size(tablename::text,'vm')) as Visability_map,
+    pg_size_pretty(pg_relation_size(tablename::text,'init')) as initialisation,
+    pg_size_pretty(
+        pg_relation_size(tablename::text,'main') +
+        pg_relation_size(tablename::text,'fsm') +
+        pg_relation_size(tablename::text,'vm') +
+        pg_relation_size(tablename::text,'init')
+    ) as total
+from (
+    select tablename
+    from pg_tables
+    where tablename like 'siri_v%'
+) as t(tablename)
+order by tablename;
+
+
+select
+    tablename,
+    indexname,
+    pg_size_pretty(pg_relation_size(indexname::text,'main')) as size
+from (
+    select tablename, indexname
+    from pg_indexes
+    where tablename in (
+        select tablename
+        from pg_tables
+        where tablename like 'siri_v%'
+    )
+) as i
+order by tablename, indexname;
